73b171a0696612514d0653410ffe6dea3dd752a3
s6319410018 <s6319410018@sau.ac.th>
2023-03-28 16:06:05 +0700

    v.1.3 by 6319410018

--- lib/views/healthy_register_ui.dart
+++ lib/views/healthy_register_ui.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HealtyRegisterUi extends StatefulWidget {
   const HealtyRegisterUi({super.key});
@@ -10,6 +12,45 @@ class HealtyRegisterUi extends StatefulWidget {
 class _HealtyRegisterUiState extends State<HealtyRegisterUi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[200],
+        title: Text(
+          'ลงทะเบียน',
+          style: GoogleFonts.kanit(),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+          child: Center(
+        child: Column(children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.width * 0.15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              CircleAvatar(
+                radius: MediaQuery.of(context).size.width * 0.15,
+                child: ClipOval(
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(FontAwesomeIcons.cameraRetro),
+              ),
+            ],
+          ),
+        ]),
+      )),
+    );
   }
 }
